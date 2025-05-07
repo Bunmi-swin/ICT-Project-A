60a82b3929664e256d9c123cf188199e19caf3e0
Olubunmi Adegbola <104935575@student.swin.edu.au>
2025-05-08 08:52:58 +1000

    Added timeout/lockout rows to users table in db

--- aged_care.sql
+++ aged_care.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 06, 2025 at 07:26 AM
+-- Generation Time: May 08, 2025 at 12:44 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -26,142 +26,56 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `appointments table`
 --
-
-CREATE TABLE `appointments table` (
-  `appointment_id` int(11) NOT NULL,
-  `resident_id` int(11) NOT NULL,
-  `service_id` int(11) NOT NULL,
-  `appointment_date` int(11) NOT NULL,
-  `time_slot` int(11) NOT NULL,
-  `status` int(11) NOT NULL,
-  `assigned_staff_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table aged_care.appointments table: #1932 - Table &#039;aged_care.appointments table&#039; doesn&#039;t exist in engine
+-- Error reading data for table aged_care.appointments table: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `aged_care`.`appointments table`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `contact us table`
 --
-
-CREATE TABLE `contact us table` (
-  `message_id` int(11) NOT NULL,
-  `sender_id` int(11) NOT NULL,
-  `recipient_id` int(11) NOT NULL,
-  `email` int(11) NOT NULL,
-  `name` int(11) NOT NULL,
-  `message_content` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table aged_care.contact us table: #1932 - Table &#039;aged_care.contact us table&#039; doesn&#039;t exist in engine
+-- Error reading data for table aged_care.contact us table: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `aged_care`.`contact us table`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `messages table`
 --
-
-CREATE TABLE `messages table` (
-  `message_id` int(11) NOT NULL,
-  `sender_id` int(11) NOT NULL,
-  `recipient_id` int(11) NOT NULL,
-  `message_content` int(11) NOT NULL,
-  `timestamp` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table aged_care.messages table: #1932 - Table &#039;aged_care.messages table&#039; doesn&#039;t exist in engine
+-- Error reading data for table aged_care.messages table: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `aged_care`.`messages table`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `resident table`
 --
-
-CREATE TABLE `resident table` (
-  `resident_id` int(11) NOT NULL,
-  `first_name` int(11) NOT NULL,
-  `last_name` int(11) NOT NULL,
-  `email` int(11) NOT NULL,
-  `password` int(11) NOT NULL,
-  `gender` int(11) NOT NULL,
-  `date_of_birth` int(11) NOT NULL,
-  `contact_number` int(11) NOT NULL,
-  `family_contact` int(11) NOT NULL,
-  `medication` int(11) NOT NULL,
-  `accessibility_requirements` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table aged_care.resident table: #1932 - Table &#039;aged_care.resident table&#039; doesn&#039;t exist in engine
+-- Error reading data for table aged_care.resident table: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `aged_care`.`resident table`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `services table`
 --
-
-CREATE TABLE `services table` (
-  `service_id` int(11) NOT NULL,
-  `service_name` int(11) NOT NULL,
-  `description` int(11) NOT NULL,
-  `duration_minutes` int(11) NOT NULL,
-  `time_slot` int(11) NOT NULL,
-  `status` int(11) NOT NULL,
-  `assigned_staff_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table aged_care.services table: #1932 - Table &#039;aged_care.services table&#039; doesn&#039;t exist in engine
+-- Error reading data for table aged_care.services table: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `aged_care`.`services table`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `staff table`
 --
-
-CREATE TABLE `staff table` (
-  `staff_id` int(11) NOT NULL,
-  `first_name` text NOT NULL,
-  `last_name` text NOT NULL,
-  `email` int(11) NOT NULL,
-  `password` int(11) NOT NULL,
-  `phone` int(11) NOT NULL,
-  `qualification` int(11) NOT NULL,
-  `employment_type` int(11) NOT NULL,
-  `salary` int(11) NOT NULL,
-  `start_date` int(11) NOT NULL,
-  `status` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table aged_care.staff table: #1932 - Table &#039;aged_care.staff table&#039; doesn&#039;t exist in engine
+-- Error reading data for table aged_care.staff table: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `aged_care`.`staff table`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
-
-CREATE TABLE `users` (
-  `ID` int(11) NOT NULL,
-  `Email` varchar(50) NOT NULL,
-  `Username` text NOT NULL,
-  `Role` enum('Admin','Resident','Family Member') NOT NULL,
-  `Password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`ID`, `Email`, `Username`, `Role`, `Password`) VALUES
-(1, 'Calmmind@gmail.com', '2025-05-01', 'Admin', '$2y$10$nXbwxhD3VX61UcJc4rY7F.eUzORWkgJbvZhQExGy0Mr1br8krneaa');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`ID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+-- Error reading structure for table aged_care.users: #1932 - Table &#039;aged_care.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table aged_care.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `aged_care`.`users`&#039; at line 1
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
